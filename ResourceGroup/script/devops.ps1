9f05bd6ce295c5f22b45acbb84e24e1e28dfd742
mukilukuna <mukilukuna@gmail.com>
2024-01-27 21:53:09 +0100

    modified:   ResourceGroup/script/devops.ps1

--- ResourceGroup/script/devops.ps1
+++ ResourceGroup/script/devops.ps1
@@ -1,17 +1,16 @@
-Param(
+param(
     $location,
     $resourceGroupName,
     $owner,
-    $costcenter,
+    $costCenter,
     $application,
     $description,
     $repo
 )
 
-
 $tag = @{
     Owner       = $owner;
-    Costcenter  = $costcenter;
+    CostCenter  = $costCenter;
     Application = $application;
     Description = $description;
     Repository  = $repo
@@ -19,16 +18,16 @@ $tag = @{
 
 try {
     Write-Host "Now creating the resource group"
-    Write-Host "Location: ${location}"
-    Write-Host "Resource group name: ${resourceGroupName}"
-    $deployment = New-AzResourceGroup -Name ${resourceGroupName} -Location ${location} -Tag ${tag}
+    Write-Host "location: ${location}"
+    Write-Host "resource group name: ${resourceGroupName}"
+    $deployment = New-AzResourceGroup -Name "${resourceGroupName}" -Location "${location}" -Tag ${tag}
     Write-Host $deployment
 }
-Catch {
+catch {
     $message = $_.Exception.Message
-    $StackTraceText = $_.Exception.StackTrace
-    Write-Host "The script failed due to the following error:"
+    $stackTraceText = $_.Exception.StackTrace
+    Write-Host "The script failed with the following text"
     Write-Host $message
-    Write-Host $StackTraceText
-    throw "Script halted"
+    Write-Host $stackTraceText
+    throw "Script Halted"
 }

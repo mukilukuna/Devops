ff6c2c2cf687864bcf3a9b62eb27528c2322dca2
mukilukuna <mukilukuna@gmail.com>
2024-04-11 21:37:48 +0200

    verdere configuratie

--- Python/Udemy/Python Mega Course/TodoApp.py
+++ Python/Udemy/Python Mega Course/TodoApp.py
@@ -1,23 +1,33 @@
 todos = []
 while True:
     gebruikers_input = input(
-        "je krijgt drie opties om uit te kiezen. die zijn: 'stop', 'toon' en 'toevoegen' 'aanpassen': ")
+        "je krijgt vier opties om uit te kiezen. die zijn: 'Compleet', 'stop', 'toon' en 'toevoegen' 'aanpassen': ")
     gebruikers_input = gebruikers_input.strip()
     match gebruikers_input:
         case "stop" | "quit":
             break
         case "toon" | "show":
-            for item in todos:
-                print(item)
+            for index, item in enumerate(todos):
+                print(f"{index + 1}, {item}")
         case "toevoegen" | "add":
             todo = input("wat is de todo: ")
             todos.append(todo.capitalize())
         case "aanpassen" | "edit":
+            for index, item in enumerate(todos):
+                print(f"{index +1}, {item}")
             number = int(
                 input("Wat is de nummer van de todo die je wilt aanpassen: "))
             number = number - 1
             new_todo = input("naar wat wil je het aanpassen?: ")
             todos[number] = new_todo
+        case "compleet" | "afronden":
+            for index, item in enumerate(todos):
+                print(f"{index + 1}, {item}")
+            keuze = int(
+                input("type de index van de item die u wilt afronden: "))
+            item_om_te_verwijderen = todos[keuze - 1]
+            todos.remove(item_om_te_verwijderen)
         case _:
             print("Jouw input wordt niet herkend door de programma, probeer het opnieuw")
-print(todos)
+for todo in enumerate(todos):
+    print(todo)

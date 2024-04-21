ebe79257d0f7981a5318e94f38e99aa7cf7bea40
mukilukuna <mukilukuna@gmail.com>
2024-04-21 22:14:06 +0200

    toevoegen van script

---
+++ powershell/Azure/Azure Virtual Desktop/GetVMAvailability.ps1
@@ -0,0 +1,19 @@
+# Vereiste modules
+$requiredModules = @("Az.Compute")
+
+# Installeer de Azure PowerShell module indien niet geïnstalleerd
+foreach ($module in $requiredModules) {
+    if (-not (Get-Module -ListAvailable -Name $module)) {
+        Write-Host "Module $module is niet geïnstalleerd. Bezig met installeren..."
+        Install-Module -Name $module -Force -AllowClobber
+    }
+    else {
+        Write-Host "Module $module is al geïnstalleerd."
+    }
+}
+
+# Importeer de module
+Import-Module Az.Compute
+
+# Uw specifieke PowerShell commando
+Get-AzComputeResourceSku | Where-Object { $_.Locations -contains "westeurope" -and $_.ResourceType -eq "virtualMachines" }
